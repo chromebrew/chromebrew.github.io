8c2a31e3a60b4ddc23c20dd811abbcd9c70933e7
supechicken <supechicken666@gmail.com>
2023-06-10 15:08:07 +0800

    Some minor adjustments

--- js/embed_template.js
+++ js/embed_template.js
@@ -4,7 +4,7 @@ const scriptElement  = document.getElementById('embed_template'),
       is_dark_mode   = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches),
       template_path  = 'https://github.com/chromebrew/chromebrew/blob/master/packages/template.rb.template',
       emgithub_style = (is_dark_mode ? 'github-dark' : 'github'),
-      emgithub_opts  = `style=${emgithub_style}&type=code&showBorder=on&showLineNumbers=on&showCopy=on`,
+      emgithub_opts  = `style=${emgithub_style}&type=code&showBorder=on&showCopy=on`,
       emgithub_link  = `https://emgithub.com/embed-v2.js?target=${encodeURIComponent(template_path)}&${emgithub_opts}`;
 
 scriptElement.src = emgithub_link;
